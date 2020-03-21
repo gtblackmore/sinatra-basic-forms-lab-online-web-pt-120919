af68f6f3ef2f79910c7db1388261ab2213fd4dc0
George Blackmore <gtblackmore@gmail.com>
2020-03-21 00:59:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+  
+  get '/create' do
+    erb :create_puppy
+  end
+  
+  post '/create' do
+  end
+
 end
